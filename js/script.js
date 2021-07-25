af1d657c461f9820d714ed953398e825edaafc70
Divins Mathew <divinsmathew@gmail.com>
2021-07-25 14:44:33 +0530

    capture

--- js/script.js
+++ js/script.js
@@ -203,10 +203,11 @@ function resizeCanvas(modelCanvas, videoWidth, videoHeight)
         var scaledWidth = modelCanvas.height * scale;
         var marginLeft = ( modelCanvas.width - scaledWidth) / 2;
 
+        resizedContext.drawImage(modelCanvas, marginLeft, 0, videoHeight, videoWidth);
+
+
         console.log('scaled: ', scaledWidth, ',', scaledHeight)
 
-
-        resizedContext.drawImage(modelCanvas, 0, 0, scaledHeight, scaledWidth);
     }
 
     return resizedCanvas.toDataURL();
