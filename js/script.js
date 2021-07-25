4e7d1808a9fcc52ca363efdfde6990aefc403dd1
Divins Mathew <divinsmathew@gmail.com>
2021-07-25 14:53:04 +0530

    capture

--- js/script.js
+++ js/script.js
@@ -219,22 +219,35 @@ function captureVideoFrame(video, format)
 
     var canvas = document.createElement("CANVAS");
 
-    canvas.width =  video.videoWidth;
-    canvas.height =  video.videoHeight;
-    canvas.getContext('2d').drawImage(video, 0, 0);
+    canvas.width = screen.width;
+    canvas.height = screen.height;
+    const imageWidth = video.videoHeight * (screen.width / screen.height);
+    canvas.getContext('2d').drawImage(
+      video,
+      (video.videoWidth - imageWidth) / 2,
+      0,
+      imageWidth,
+      video.videoHeight,
+      0,
+      0,
+      screen.width,
+      screen.height,
+    );
     var dataUri = canvas.toDataURL('image/' + format);
     var data = dataUri.split(',')[1];
     var mimeType = dataUri.split(';')[0].slice(5)
-
+  
     var bytes = window.atob(data);
     var buf = new ArrayBuffer(bytes.length);
     var arr = new Uint8Array(buf);
-
-    for (var i = 0; i < bytes.length; i++)
-        arr[i] = bytes.charCodeAt(i);
-
-    var blob = new Blob([arr], { type: mimeType });
-    return { blob: blob, dataUri: dataUri, format: format, width: canvas.width, height: canvas.height };
+  
+    for (var i = 0; i < bytes.length; i++) {
+      arr[i] = bytes.charCodeAt(i);
+    }
+  
+    var blob = new Blob([ arr ], { type: mimeType });
+    canvas.remove();
+    return { blob: blob, dataUri: dataUri, format: format, width: screen.width, height: screen.height };
 };
 
 const defaultOptions = {
