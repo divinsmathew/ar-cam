9be9d38653f657e25369d3f72f7d78d59d577709
Divins Mathew <divinsmathew@gmail.com>
2021-07-25 13:08:52 +0530

    capture

--- js/script.js
+++ js/script.js
@@ -152,55 +152,66 @@ function toggleRotate()
     rotateMode = !rotateMode;
 }
 
-function getSnap()
+const getSnap = async () => 
 {
-    // let canvas = document.querySelector('body > a-scene > canvas');
-    // var link = document.getElementById('link');
-    // link.setAttribute('download', 'MintyPaper.png');
-    // link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
-    // link.click();
+
+
+    // html2canvas(document.body).then(function(canvas) {
+    //     //document.body.appendChild(canvas);
+
+    //     var link = document.getElementById('link');
+    //     link.setAttribute('download', 'MintyPaper.png');
+    //     link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+    //     link.click();
+    // })
 
 
     let aScene = document
-		.querySelector("a-scene")
-		.components.screenshot.getCanvas("perspective");
-	let frame = captureVideoFrame("video", "png");
-	aScene = resizeCanvas(aScene, frame.width, frame.height);
-	frame = frame.dataUri;
-	mergeImages([frame, aScene]).then(b64 => {
-		let link = document.getElementById("link", "png");
-		link.setAttribute("download", "AR.png");
-		link.setAttribute("href", b64);
-		link.click();
-	});
+        .querySelector("a-scene")
+        .components.screenshot.getCanvas("perspective");
+    let frame = captureVideoFrame("video", "png");
+    aScene = resizeCanvas(aScene, frame.width, frame.height);
+    frame = frame.dataUri;
+    mergeImages([frame, aScene]).then(b64 =>
+    {
+        let link = document.getElementById("link", "png");
+        link.setAttribute("download", new Date().toLocaleString().replaceAll(':', '-').replaceAll('/', '-') + " AR.png");
+        link.setAttribute("href", b64);
+        link.click();
+    });
 }
 
-function resizeCanvas(origCanvas, width, height) {
-	let resizedCanvas = document.createElement("canvas");
-	let resizedContext = resizedCanvas.getContext("2d");
+function resizeCanvas(origCanvas, width, height)
+{
+    let resizedCanvas = document.createElement("canvas");
+    let resizedContext = resizedCanvas.getContext("2d");
 
-	resizedCanvas.height = width;
-	resizedCanvas.width = height;
+    resizedCanvas.height = height;
+    resizedCanvas.width = width;
 
-	resizedContext.drawImage(origCanvas, 0, 0, width, height);
-	return resizedCanvas.toDataURL();
+    if (width > height) {
+        // Landscape
+        resizedContext.drawImage(origCanvas, 0, 0, width, height);
+    } else {
+        // Portrait
+        var scale = height / width;
+        var scaledHeight = origCanvas.width * scale;
+        var scaledWidth = origCanvas.height * scale;
+        var marginLeft = ( origCanvas.width - scaledWidth) / 2;
+        resizedContext.drawImage(origCanvas, marginLeft, 0, scaledWidth, scaledHeight);
+    }
+
+    return resizedCanvas.toDataURL();
 }
 
-function captureVideoFrame(video, format, width, height) {
-    if (typeof video === 'string') {
-        video = document.querySelector(video);
-    }
-
-    format = format || 'jpeg';
-
-    if (!video || (format !== 'png' && format !== 'jpeg')) {
-        return false;
-    }
+function captureVideoFrame(video, format)
+{
+    video = document.querySelector(video);
 
     var canvas = document.createElement("CANVAS");
 
-    canvas.width = width || video.videoWidth;
-    canvas.height = height || video.videoHeight;
+    canvas.width =  video.videoWidth;
+    canvas.height =  video.videoHeight;
     canvas.getContext('2d').drawImage(video, 0, 0);
     var dataUri = canvas.toDataURL('image/' + format);
     var data = dataUri.split(',')[1];
@@ -210,85 +221,89 @@ function captureVideoFrame(video, format, width, height) {
     var buf = new ArrayBuffer(bytes.length);
     var arr = new Uint8Array(buf);
 
-    for (var i = 0; i < bytes.length; i++) {
+    for (var i = 0; i < bytes.length; i++)
         arr[i] = bytes.charCodeAt(i);
-    }
 
-    var blob = new Blob([ arr ], { type: mimeType });
+    var blob = new Blob([arr], { type: mimeType });
     return { blob: blob, dataUri: dataUri, format: format, width: canvas.width, height: canvas.height };
 };
 
-
-
-
 const defaultOptions = {
-	format: 'image/png',
-	quality: 0.92,
-	width: undefined,
-	height: undefined,
-	Canvas: undefined,
-	crossOrigin: undefined
+    format: 'image/png',
+    quality: 0.92,
+    width: undefined,
+    height: undefined,
+    Canvas: undefined,
+    crossOrigin: undefined
 };
 
-// Return Promise
-const mergeImages = (sources = [], options = {}) => new Promise(resolve => {
-	options = Object.assign({}, defaultOptions, options);
+const mergeImages = (sources = [], options = {}) => new Promise(resolve =>
+{
+    options = Object.assign({}, defaultOptions, options);
 
-	// Setup browser/Node.js specific variables
-	const canvas = options.Canvas ? new options.Canvas() : window.document.createElement('canvas');
-	const Image = options.Image || window.Image;
+    // Setup browser/Node.js specific variables
+    const canvas = options.Canvas ? new options.Canvas() : window.document.createElement('canvas');
+    const Image = options.Image || window.Image;
 
-	// Load sources
-	const images = sources.map(source => new Promise((resolve, reject) => {
-		// Convert sources to objects
-		if (source.constructor.name !== 'Object') {
-			source = { src: source };
-		}
+    // Load sources
+    const images = sources.map(source => new Promise((resolve, reject) =>
+    {
+        // Convert sources to objects
+        if (source.constructor.name !== 'Object')
+        {
+            source = { src: source };
+        }
 
-		// Resolve source and img when loaded
-		const img = new Image();
-		img.crossOrigin = options.crossOrigin;
-		img.onerror = () => reject(new Error('Couldn\'t load image'));
-		img.onload = () => resolve(Object.assign({}, source, { img }));
-		img.src = source.src;
-	}));
+        // Resolve source and img when loaded
+        const img = new Image();
+        img.crossOrigin = options.crossOrigin;
+        img.onerror = () => reject(new Error('Couldn\'t load image'));
+        img.onload = () => resolve(Object.assign({}, source, { img }));
+        img.src = source.src;
+    }));
 
-	// Get canvas context
-	const ctx = canvas.getContext('2d');
+    // Get canvas context
+    const ctx = canvas.getContext('2d');
 
-	// When sources have loaded
-	resolve(Promise.all(images)
-		.then(images => {
-			// Set canvas dimensions
-			const getSize = dim => options[dim] || Math.max(...images.map(image => image.img[dim]));
-			canvas.width = getSize('width');
-			canvas.height = getSize('height');
+    // When sources have loaded
+    resolve(Promise.all(images)
+        .then(images =>
+        {
+            // Set canvas dimensions
+            const getSize = dim => options[dim] || Math.max(...images.map(image => image.img[dim]));
+            canvas.width = getSize('width');
+            canvas.height = getSize('height');
 
-			// Draw images to canvas
-			images.forEach(image => {
-				ctx.globalAlpha = image.opacity ? image.opacity : 1;
-				return ctx.drawImage(image.img, image.x || 0, image.y || 0);
-			});
+            // Draw images to canvas
+            images.forEach(image =>
+            {
+                ctx.globalAlpha = image.opacity ? image.opacity : 1;
+                return ctx.drawImage(image.img, image.x || 0, image.y || 0);
+            });
 
-			if (options.Canvas && options.format === 'image/jpeg') {
-				// Resolve data URI for node-canvas jpeg async
-				return new Promise((resolve, reject) => {
-					canvas.toDataURL(options.format, {
-						quality: options.quality,
-						progressive: false
-					}, (err, jpeg) => {
-						if (err) {
-							reject(err);
-							return;
-						}
-						resolve(jpeg);
-					});
-				});
-			}
+            if (options.Canvas && options.format === 'image/jpeg')
+            {
+                // Resolve data URI for node-canvas jpeg async
+                return new Promise((resolve, reject) =>
+                {
+                    canvas.toDataURL(options.format, {
+                        quality: options.quality,
+                        progressive: false
+                    }, (err, jpeg) =>
+                    {
+                        if (err)
+                        {
+                            reject(err);
+                            return;
+                        }
+                        resolve(jpeg);
+                    });
+                });
+            }
 
-			// Resolve all other data URIs sync
-			return canvas.toDataURL(options.format, options.quality);
-		}));
+            // Resolve all other data URIs sync
+            return canvas.toDataURL(options.format, options.quality);
+        }));
 });
 
 
