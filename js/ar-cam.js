a5235d28fbcf62e5fac79541648eed5382122e07
Divins Mathew <divinsmathew@gmail.com>
2021-08-19 02:15:17 +0530

    -revert

--- js/ar-cam.js
+++ js/ar-cam.js
@@ -67,7 +67,7 @@ window.onload = () =>
     let pinch = new ZingTouch.Distance();
     activeRegion.bind(containerElement, pinch, function (event)
     {
-       // if (rotateMode || moveMode) return;
+        if (rotateMode || moveMode) return;
 
         let factor = event.detail.change / 500.0;
         let scale = entity.getAttribute('scale').x;
@@ -76,16 +76,14 @@ window.onload = () =>
         entity.object3D.scale.set(scale, scale, scale)
         // log.innerText = factor;
     });
-
-
     let swipe = new ZingTouch.Pan({
         numInputs: 1,
         threshold: 5
     })
-
-    //move
     activeRegion.bind(containerElement, swipe, function (event)
     {
+        if (moveMode)
+        {
             let position = entity.getAttribute('position');
             let direction = calculateDirection(event.detail.data[0].currentDirection);
 
@@ -100,33 +98,27 @@ window.onload = () =>
             }
 
             entity.object3D.position.set(position.x, position.y, position.z);
-    });
-
-    let twoFingerSwipe = new ZingTouch.Pan({
-        numInputs: 2,
-        threshold: 5
-    })
-
-    //rotate
-    activeRegion.bind(containerElement, twoFingerSwipe, function (event)
-    {
-        let rotation = entity.getAttribute('rotation');
-        let direction = calculateDirection(event.detail.data[0].currentDirection);
-
-        if (!direction) return;
-
-        switch (direction)
-        {
-            case 'up': rotation.x -= 1.5; break;
-            case 'down': rotation.x += 1.5; break;
-            case 'left': rotation.y -= 1.5; break;
-            case 'right': rotation.y += 1.5; break;
         }
+        else if (rotateMode)
+        {
+            let rotation = entity.getAttribute('rotation');
+            let direction = calculateDirection(event.detail.data[0].currentDirection);
 
-        entity.object3D.rotation.set(
-            THREE.Math.degToRad(rotation.x),
-            THREE.Math.degToRad(rotation.y),
-            THREE.Math.degToRad(rotation.z)
-        );
+            if (!direction) return;
+
+            switch (direction)
+            {
+                case 'up': rotation.x -= 1.5; break;
+                case 'down': rotation.x += 1.5; break;
+                case 'left': rotation.y -= 1.5; break;
+                case 'right': rotation.y += 1.5; break;
+            }
+
+            entity.object3D.rotation.set(
+                THREE.Math.degToRad(rotation.x),
+                THREE.Math.degToRad(rotation.y),
+                THREE.Math.degToRad(rotation.z)
+            );
+        }
     });
 }
